1c7f815928837e35784b8daae62440ec33f5a1c1
Harsh Vashishta <harshvashishta7@gmail.com>
2024-01-15 15:23:45 +0530

    minor changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -244,7 +244,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res
         .status(200)
-        .json(200, req.user, "current user fetched succesfully")
+        .json(new ApiResponse(200, req.user, "current user fetched succesfully"))
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
